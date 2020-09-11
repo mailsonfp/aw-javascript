527d394f69ceab40555d21b8842b218e5f4242eb
Mailson Fernando Pereira <mailsonfp.dev@gmail.com>
2020-09-10 23:56:50 -0300

    Aula 5 e 6

---
+++ arrays.js
@@ -0,0 +1,29 @@
+var emails = [];
+emails[0] = 'joao@email.com';
+emails[1] = 'maria@email.com';
+
+console.log('emails[0]', emails[0]);
+console.log('emails[5]', emails[5]);
+
+var nomes = ['João', 'Maria'];
+console.log('Quantos nomes?', nomes.length);
+
+nomes[4] = 'José';
+
+console.log('Quantos nomes depois do José?', nomes.length);
+console.log('nomes[3]', nomes[3]);
+
+emails.push('mailsonfp.dev@gmail.com');
+console.log('Quantos emails?', emails.length);
+
+var emailRemovido = emails.pop();
+console.log('email removido: ', emailRemovido);
+console.log('Quantos emails?', emails.length);
+
+console.log('inserindo no começo do array unshift');
+emails.unshift('mailsonfp.dev@gmail.com');
+console.log(emails);
+
+console.log('removendo primeira entrada do array: shift');
+emailRemovido = emails.shift();
+console.log('email removido: ', emailRemovido);
