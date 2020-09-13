4f34e075593e1ccd667ee9df6bca98289eda73d8
Mailson Fernando Pereira <mailsonfp.dev@gmail.com>
2020-09-12 23:22:28 -0300

    Concluindo o curso

---
+++ jquery/disparando-eventos.js
@@ -0,0 +1,26 @@
+$(function() {
+	var atividade = $('#atividade');
+	var listaAtividades = $('#lista-atividades');
+
+	atividade.on('keyup', function(e) {
+		var keyCode = e.keyCode;
+
+		if (keyCode === 13) {
+			atividade.trigger('enter', atividade.val());
+			atividade.val('');
+		}
+	});
+
+	atividade.on('enter', function(evento, texto) {
+		listaAtividades.prepend('<li>' + texto + '</li>');
+	});
+
+	atividade.on('enter', function(eventp, texto) {
+		console.log('Outra funcionalidade aqui...');
+    });
+    
+    $('#botao1').on('click', function(e) {
+        listaAtividades.empty();     
+	});
+
+});
