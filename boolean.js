55f5ddc350d4b631466e7735ef76e86418e7d632
Mailson Fernando Pereira <mailsonfp.dev@gmail.com>
2020-09-13 22:39:10 -0300

    Incrementando comparações booleanas

--- boolean.js
+++ boolean.js
@@ -1,13 +1,13 @@
 var x = 1;
 do{
 x = 2;
-console .log(0 ? 'É true' : 'É false');
-console.log(!0 ? 'É true' : 'É false');
-console.log(!!0 ? 'É true' : 'É false');
-console.log('' ? 'É true' : 'É false');
-console.log(NaN ? 'É true' : 'É false');
-console.log( null ? 'É true' : 'É false');
-console.log( undefined ? 'É true' : 'É false');
-console.log( false ? 'É true' : 'É false');
-console.log( 1===1 ? 'É true' : 'É false');
+console.log('Zero:', 0 ? 'É true' : 'É false');
+console.log('Negando Zero:', !0 ? 'É true' : 'É false');
+console.log('Negação dupla de zero:', !!0 ? 'É true' : 'É false');
+console.log('String vazia:', '' ? 'É true' : 'É false');
+console.log('Not a Number:', NaN ? 'É true' : 'É false');
+console.log('null:', null ? 'É true' : 'É false');
+console.log('undefined:', undefined ? 'É true' : 'É false');
+console.log('false:', false ? 'É true' : 'É false');
+console.log('1===1:', 1===1 ? 'É true' : 'É false');
 } while(x===1);
